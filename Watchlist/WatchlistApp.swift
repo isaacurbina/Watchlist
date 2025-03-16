20e7a4b43de3da428407552a0d1f3767c16fed7e
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-03-15 22:49:48 -0500

    first commit

---
+++ Watchlist/WatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchlistApp.swift
+//  Watchlist
+//
+//  Created by Isaac Urbina on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
