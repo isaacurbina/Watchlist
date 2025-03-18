9cf16e554478ef6c608c86251a2b0ff20b993b38
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-03-17 19:15:22 -0500

    58. 2. Empty List View

--- Watchlist/View/ContentView.swift
+++ Watchlist/View/ContentView.swift
@@ -6,19 +6,28 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+	
+	// MARK: - Properties
+	
+	@Environment(\.modelContext) var modelContext
+	@Query private var movies: [Movie]
+	
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		List {
+			
+		}
+		.overlay {
+			if movies.isEmpty {
+				EmptyListView()
+			}
+		}
     }
 }
 
 #Preview {
     ContentView()
+		.modelContainer(for: Movie.self, inMemory: true)
 }
