58e54f7b7035947552380181080ce69b8231a99c
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-03-17 19:28:04 -0500

    59. 3. New Movie Form View

---
+++ Watchlist/View/NewMovieFormView.swift
@@ -0,0 +1,90 @@
+//
+//  NewMovieFormView.swift
+//  Watchlist
+//
+//  Created by Isaac Urbina on 3/17/25.
+//
+
+import SwiftUI
+
+struct NewMovieFormView: View {
+	
+	// MARK: - properties
+	
+	@Environment(\.modelContext) var modelContext
+	@Environment(\.dismiss) var dismiss
+	
+	@State private var title: String = ""
+	@State private var selectedGenre: Genre = .kids
+	
+	
+	// MARK: - functions
+	private func addNewMovie() {
+		let newMovie = Movie(title: title, genre: selectedGenre)
+		modelContext.insert(newMovie)
+		title = ""
+		selectedGenre = .kids
+	}
+	
+	
+	// MARK: - views
+	
+    var body: some View {
+		Form {
+			List {
+				
+				
+				// MARK: - header
+				Text("What to Watch")
+					.font(.largeTitle.weight(.black))
+					.foregroundStyle(.blue.gradient)
+					.frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+					.padding(.vertical)
+				
+				
+				// MARK: - title
+				TextField("Movie Title", text: $title)
+					.textFieldStyle(.roundedBorder)
+					.font(.largeTitle.weight(.light))
+				
+				// MARK: - genre
+				Picker("Genre", selection: $selectedGenre) {
+					ForEach(Genre.allCases) { genre in
+						Text(genre.name)
+							.tag(genre)
+					}
+				}
+				
+				
+				// MARK: - save button
+				Button {
+					addNewMovie()
+					dismiss()
+					
+				} label: {
+					Text("Save")
+						.font(.title2.weight(.medium))
+						.frame(maxWidth: .infinity)
+				}
+				.buttonStyle(.borderedProminent)
+				.controlSize(.extraLarge)
+				.buttonBorderShape(.roundedRectangle)
+				
+				
+				// MARK: - cancel button
+				Button {
+					dismiss()
+					
+				} label: {
+					Text("Close")
+						.frame(maxWidth: .infinity)
+				}
+			}
+			.listRowSeparator(.hidden)
+		}
+    }
+}
+
+#Preview {
+    NewMovieFormView()
+}
